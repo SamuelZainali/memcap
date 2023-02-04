081e3b3331d7db6c5de8849a6a1f76ec1b10acad
SamuelZainali <115083138+SamuelZainali@users.noreply.github.com>
2023-02-04 21:44:18 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFF500",
+        "secondary-1": "#973EFF",
+        "secondary-2": "#B30044",
+        "tertiary-1": "#FF4B93",
+        "tertiary-2": "#00B308",
+      },
+    },
+  },
+  plugins: [],
+};
